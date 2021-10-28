18ace4c945a19bdfe07bfb5ff7936329a8d1f81a
Ammon Nelson <Ammon1nelson@gmail.com>
2021-10-28 14:40:36 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Overview for Flappy Bird Clone
+This project helped me to expand my skill set into the mobile application world and helped me learn more about javascript and game logic.
+
+The point of this app is to provide entertainment for people. Flappy birds objective is to fly through obstacles to see how far you can make it without hitting anything, and when you do the game is over.
+
+The bigger purpose of this project was to expand my knowledge with coding and posing a challenge inside building apps
+
+Youtube Demo: [Youtube Demo](https://youtu.be/eJ9DmWVx3kg)
+
+# Development Environment
+I used VS Code as my text editor! 
+I used EXPO to help me create the app. Expo does alot of the leg work for you when you are developing the app which is really nice. It has really good documentation as well and i will leave that downbelow to help those that are in need of that. 
+
+# Useful Websites
+* [EXPO documentation](https://docs.expo.dev/)
+* [YouTube](https://www.youtube.com/) Look up guides! It helps 
+* [JavaScript Tutor](https://www.w3schools.com/js/DEFAULT.asp)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+* I can add cool graphics instead of having just the boxes. 
+* I would like to add a feature that speeds the game up as the player scores more points but it would only get so fast.
+* Saving high scores on the local device. 
---
+++ entities/index.js
@@ -0,0 +1,35 @@
+import Matter from "matter-js"
+import Bird from "../components/Bird";
+import Floor from "../components/Floor";
+import Obstacle from "../components/Obstacle";
+
+import { Dimensions } from 'react-native'
+import { getPipeSizePosPair } from "../utils/random";
+
+const windowHeight = Dimensions.get('window').height
+const windowWidth = Dimensions.get('window').width
+
+
+export default restart => {
+    let engine = Matter.Engine.create({ enableSleeping: false })
+
+    let world = engine.world
+
+    world.gravity.y = .5;
+
+    const pipeSizePosA = getPipeSizePosPair()
+    const pipeSizePosB = getPipeSizePosPair(windowWidth * 0.9)
+    return {
+        physics: { engine, world },
+
+        Bird: Bird(world, 'black', { x: 60, y: 400 }, { height: 65, width: 65 }),
+
+        ObstacleTop1: Obstacle(world, 'ObstacleTop1', 'red', pipeSizePosA.pipeTop.pos, pipeSizePosA.pipeTop.size),
+        ObstacleBottom1: Obstacle(world, 'ObstacleBottom1', 'blue', pipeSizePosA.pipeBottom.pos, pipeSizePosA.pipeBottom.size),
+
+        ObstacleTop2: Obstacle(world, 'ObstacleTop2', 'blue', pipeSizePosB.pipeTop.pos, pipeSizePosB.pipeTop.size),
+        ObstacleBottom2: Obstacle(world, 'ObstacleBottom2', 'red', pipeSizePosB.pipeBottom.pos, pipeSizePosB.pipeBottom.size),
+
+        Floor: Floor(world, 'grey', { x: windowWidth / 2, y: windowHeight }, { height: 50, width: windowWidth })
+    }
+}
